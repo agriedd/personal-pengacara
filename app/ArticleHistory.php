1a344558539e7509f2209b2968ed9fa3e5a6d21d
Agri Edd. Oematan <dnr739528@gmail.com>
2020-06-08 20:42:50 +0800

    add relationship belongsTo articles on ArticleHistory Model

--- app/ArticleHistory.php
+++ app/ArticleHistory.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ArticleHistory extends Model
 {
-    //
+    public function article(){
+        return $this->belongsTo(Article::class, 'id_article', 'id');
+    }
 }
