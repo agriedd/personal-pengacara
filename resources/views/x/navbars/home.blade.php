9ec2b005090c837a383ab9417eb03d98879e6984
Agri Edd. Oematan <dnr739528@gmail.com>
2020-07-10 16:22:13 +0800

    fixed home navbar

--- resources/views/x/navbars/home.blade.php
+++ resources/views/x/navbars/home.blade.php
@@ -1,16 +1,19 @@
+@php
+    $home = !!preg_match("/^(\/|home)$/", request()->route()->uri());
+@endphp
 <!-- Right Side Of Navbar -->
     <!-- Authentication Links -->
     <li class="nav-item px-1">
         <a class="nav-link text-dark h6 rounded" href="{{ route('home') }}">Halaman Awal</a>
     </li>
     <li class="nav-item px-1">
-        <a class="nav-link text-dark h6 rounded" href="#profil" @click="navbar.toggleCollapse('menuHome', false)">Profil</a>
+        <a class="nav-link text-dark h6 rounded" href="{{ $home ? "#profil" : route('home', '#profil') }}" @click="navbar.toggleCollapse('menuHome', false)">Profil</a>
     </li>
     <li class="nav-item px-1">
         <a class="nav-link text-dark h6 rounded" href="{{ route('home.artikel') }}">Artikel</a>
     </li>
     <li class="nav-item px-1">
-        <a class="nav-link text-dark h6 rounded" href="#bahan-hukum" @click="navbar.toggleCollapse('menuHome', false)">Bahan Hukum</a>
+        <a class="nav-link text-dark h6 rounded" href="{{ $home ? "#bahan-hukum" : route('home', '#bahan-hukum') }}" @click="navbar.toggleCollapse('menuHome', false)">Bahan Hukum</a>
     </li>
     <li class="nav-item px-1">
         <a class="nav-link text-dark h6 rounded" href="{{ route('home.galeri') }}" @click="navbar.toggleCollapse('menuHome', false)">Galeri</a>
